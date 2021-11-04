59d33fe82296767df19afa471f2e2d4bcc654ddd
Beatriz Souza <beatrizgsouza.bgs@gmail.com>
2021-11-04 13:39:43 -0300

    Projeto Integrador || Generation

---
+++ Techdelas/TechdElas - Spring/src/main/java/org/generation/techdelas/TechdelasApplication.java
@@ -0,0 +1,13 @@
+package org.generation.techdelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechdelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechdelasApplication.class, args);
+	}
+
+}
---
+++ Techdelas/TechdElas - Spring/src/main/java/org/generation/techdelas/model/Tema.java
@@ -0,0 +1,70 @@
+package org.generation.techdelas.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table (name = "tb_tema")
+public class Tema {
+
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id_tema;
+		
+		@NotNull
+		@Size(min = 5, max = 40, message = "O número mínimo de caracteres é 5 e o máximo 40.")
+		private String nome_tema;
+		
+		@NotNull
+		@Size (min = 10 , max = 300, message = "O número mínimo de caracteres é 10 e o máximo é 300.")
+		private String sobre;
+		
+		@NotNull
+		@Size (min = 10, max = 255, message = "O número mínimo de caracteres é 10 e o máximo é 255.")
+		private String img_devs;
+		
+		
+		public long getId_tema() {
+			return id_tema;
+		}
+
+		public void setId_tema(long id_tema) {
+			this.id_tema = id_tema;
+		}
+
+		public String getNome_tema() {
+			return nome_tema;
+		}
+
+		public void setNome_tema(String nome_tema) {
+			this.nome_tema = nome_tema;
+		}
+
+		public String getSobre() {
+			return sobre;
+		}
+
+		public void setSobre(String sobre) {
+			this.sobre = sobre;
+		}
+
+		public String getImg_devs() {
+			return img_devs;
+		}
+
+		public void setImg_devs(String img_devs) {
+			this.img_devs = img_devs;
+		}
+
+				
+		
+	}
+
